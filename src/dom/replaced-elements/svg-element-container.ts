f9fe3fa080575fb16d612cf9016945546a5d76df
Dmitry Naumchik <ceqadrat@gmail.com>
2021-07-02 11:37:10 +0300

    Fix encoding SVG

--- src/dom/replaced-elements/svg-element-container.ts
+++ src/dom/replaced-elements/svg-element-container.ts
@@ -9,7 +9,8 @@ export class SVGElementContainer extends ElementContainer {
     constructor(img: SVGSVGElement) {
         super(img);
         const s = new XMLSerializer();
-        this.svg = `data:image/svg+xml,${encodeURIComponent(s.serializeToString(img))}`;
+        const encodeSvg = window.btoa ? window.btoa : encodeURIComponent;
+        this.svg = `data:image/svg+xml;base64,${encodeSvg(s.serializeToString(img))}`;
         this.intrinsicWidth = img.width.baseVal.value;
         this.intrinsicHeight = img.height.baseVal.value;
 
